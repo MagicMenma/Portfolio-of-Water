7e9ea0126168100c1d4b366b43dadc81ba2a5c92
magicmenma <magicmenma@gmail.com>
2024-01-08 19:13:17 -0800

    Major Update SoicalBfly
    2.2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-// script.js
-$(function() {
+﻿$(function() {
     $.scrollify({
-    section : ".scrollPart",
-    scrollbars: true,
-    setHeights: false,
+        section : ".scrollPart",
+        scrollbars: true,
+        setHeights: false,
     });
 });
 
- window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function() {
     if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
         var header = document.querySelector('.header_section');
         if (header) {
@@ -15,15 +14,45 @@ $(function() {
         }
     } else {
         header = document.querySelector('.header_section');
-        header.style.background = 'rgba(241, 231, 217, 0.7)';
+        if (header) {
+            header.style.background = 'rgba(241, 231, 217, 0.7)';
+        }
     }
 });
 
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('socialButterflyYoutubeVideo', {
+        height: '100%',
+        width: '100%',
+        videoId: 'zDGY77uq-0U',    
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+var isVideoPlaying = false;
+function onPlayerStateChange(event) {
+    if(event.data === YT.PlayerState.PLAYING) {
+        isVideoPlaying = true;
+    } else {
+        isVideoPlaying = false;
+    }
+    console.log('Video playing status changed:', isVideoPlaying);
+}
+
 var counter = 2;
 setInterval(function(){
+    console.log('Checking if video is playing:', isVideoPlaying);
+    
+    if(isVideoPlaying) {
+        return;
+    }
+
     for (var i = 1; i <= 5; i++) {
         var radio = document.getElementById('radio' + i);
-        if (radio.checked) {
+        if (radio && radio.checked) {
             counter = parseInt(radio.id.replace('radio', ''));
             break;
         }
@@ -32,5 +61,8 @@ setInterval(function(){
     if(counter > 5){
         counter = 1;
     }
-    document.getElementById('radio' + counter).checked = true;
+    var nextRadio = document.getElementById('radio' + counter);
+    if (nextRadio) {
+        nextRadio.checked = true;
+    }
 }, 6000);
