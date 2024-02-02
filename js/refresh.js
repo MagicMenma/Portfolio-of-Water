e21feb3f54e64b77a8f3da2de599ad76bab52867
magicmenma <magicmenma@gmail.com>
2024-02-01 19:54:30 -0800

    The Sunset Fix
    2.7

--- js/refresh.js
+++ js/refresh.js
@@ -2,5 +2,7 @@
     if (!localStorage.getItem('page_loaded')) {
         localStorage.setItem('page_loaded', 'true');
         location.reload();
-    } 
+    } else {
+        localStorage.removeItem('page_loaded');
+    }
 };
