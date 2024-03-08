0abdfb7f68c6864a2519d967c519b5ec10a4d16c
magicmenma <magicmenma@gmail.com>
2024-03-08 03:52:45 -0800

    MAT 1.1

--- js/3D.js
+++ js/3D.js
@@ -1,53 +1,26 @@
-﻿// Import the TFRFF.js library
-import * as THREE from 'three';
-// Addons
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+﻿import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-let object;
-
-let controls
-
-let objToRender = 'Head';
-
-const loader = new GLTFLoader();
-
-// Load a glTF resource
-loader.load(
-	// resource URL
-	'models/Head/Head.glb',
-	// called when the resource is loaded
-	function ( gltf ) {
-		object = gltf.scene;
-        scene.add(object);
-	},
-	// called while loading is progressing
-	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-	},
-	// called when loading has errors
-	function ( error ) {
-		console.log( 'An error happened' );
-	}
-);
-
-const renderer = new THREE.WebGLRenderer({alpha: true});
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
-}
 
-window.addEventListener("resize", function(){
-    camera.aspect = this.window.innerWidth / this.window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
 
 animate();
