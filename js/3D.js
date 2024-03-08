f6648452ec4d09119e0edf650654b7f47ff51a79
magicmenma <magicmenma@gmail.com>
2024-03-08 07:17:39 -0800

    MAT1.36
    w

--- js/3D.js
+++ js/3D.js
@@ -7,8 +7,10 @@ import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/js
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
+scene.background = new Color('skyblue');
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0, 0, 10);
 
 const container = document.querySelector('#scene-container');
 
@@ -21,6 +23,7 @@ loader.load( 'models/Head/Head.glb',
 function ( gltf ) {
 	objectHead = gltf.scene;
   scene.add( objectHead );
+
   if(objectHead != null){
     console.log('Object Loaded');
   }else{
@@ -35,20 +38,18 @@ undefined, function ( error ) {
 } );
 
 //const renderer = new THREE.WebGLRenderer({alpha: true});
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+const renderer = new WebGLRenderer();
+renderer.setSize( container.clientWidth, container.clientHeight );
 
-camera.position.z = 5;
+// function animate(){
+//   requestAnimationFrame(animate);
+// }
 
-function animate(){
-  requestAnimationFrame(animate);
-}
-
-window.addEventListener("resize", function(){
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
+// window.addEventListener("resize", function(){
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+// });
 
 animate();
 
