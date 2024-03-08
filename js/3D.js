2d8a1872ec717c910e8f91689b8bc116126835da
magicmenma <magicmenma@gmail.com>
2024-03-08 03:40:03 -0800

    Model Addons Test
    2.9

---
+++ js/3D.js
@@ -0,0 +1,53 @@
+﻿// Import the TFRFF.js library
+import * as THREE from 'three';
+// Addons
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+let object;
+
+let controls
+
+let objToRender = 'Head';
+
+const loader = new GLTFLoader();
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'models/Head/Head.glb',
+	// called when the resource is loaded
+	function ( gltf ) {
+		object = gltf.scene;
+        scene.add(object);
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	},
+	// called when loading has errors
+	function ( error ) {
+		console.log( 'An error happened' );
+	}
+);
+
+const renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+}
+
+window.addEventListener("resize", function(){
+    camera.aspect = this.window.innerWidth / this.window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
