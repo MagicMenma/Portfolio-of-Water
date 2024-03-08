34861e846cfb63527579644fd37580cada058a03
magicmenma <magicmenma@gmail.com>
2024-03-08 05:45:06 -0800

    MAT1.20
    w

--- js/3D.js
+++ js/3D.js
@@ -1,60 +1,49 @@
 ﻿// Import the TFRFF.js library
 import * as THREE from "https://cdn.skypack.dev/three@0.132.2/build/three.module.js";
 
-import {
-  BoxBufferGeometry,
-  Color,
-  Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-} from "https://cdn.skypack.dev/three@0.132.2";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js";
 
-// Get a reference to the container element that will hold our scene
 const container = document.querySelector('#scene-container');
 
-// create a Scene
-const scene = new Scene();
+const scene = new THREE.Scene();
 
-// Set the background color
-scene.background = new Color('skyblue');
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-// Create a camera
-const fov = 35; // AKA Field of View
-const aspect = container.clientWidth / container.clientHeight;
-const near = 0.1; // the near clipping plane
-const far = 100; // the far clipping plane
+let object;
 
-const camera = new PerspectiveCamera(fov, aspect, near, far);
+let controls
 
-// every object is initially created at ( 0, 0, 0 )
-// move the camera back so we can view the scene
-camera.position.set(0, 0, 10);
+const loader = new GLTFLoader();
+loader.load( 'models/Head/Head.glb', function ( gltf ) {
+	object = gltf.scene;
+  scene.add( object );
+}, 
+function (xhr){
+  console.log((xhr.loader / xhr.total * 100) + '% loader');
+},
+undefined, function ( error ) {
+	console.error( error );
+} );
 
-// create a geometry
-const geometry = new BoxBufferGeometry(2, 2, 2);
+const renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize( window.innerWidth, window.innerHeight );
 
-// create a default (white) Basic material
-const material = new MeshBasicMaterial();
-
-// create a Mesh containing the geometry and material
-const cube = new Mesh(geometry, material);
-
-// add the mesh to the scene
-scene.add(cube);
-
-// create the renderer
-const renderer = new WebGLRenderer();
-
-// next, set the renderer to the same size as our container element
-renderer.setSize(container.clientWidth, container.clientHeight);
-
-// finally, set the pixel ratio so that our scene will look good on HiDPI displays
-renderer.setPixelRatio(window.devicePixelRatio);
-
-// add the automatically created <canvas> element to the page
+//render place
 container.append(renderer.domElement);
 
-// render, or 'create a still image', of the scene
-renderer.render(scene, camera);
+camera.position.z = 5;
+
+function animate(){
+  requestAnimationFrame(animate);
+}
+
+window.addEventListener("resize", function(){
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
