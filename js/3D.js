310977562d6a677fa1d5a0776cbffa0438f4dccb
magicmenma <magicmenma@gmail.com>
2024-03-08 04:49:23 -0800

    MAT1.14
    w

--- js/3D.js
+++ js/3D.js
@@ -1,4 +1,4 @@
-﻿import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/0.162.0/three.module.js";
+﻿import * as THREE from "https://cdn.skypack.dev/three@0.132.2/build/three.module.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
