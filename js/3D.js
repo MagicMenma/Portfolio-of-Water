090d7ce33438e78d716e622cf5a8a81ac0f4ed82
magicmenma <magicmenma@gmail.com>
2024-03-08 17:58:34 -0800

    Model Addons Basic
    2.22

--- js/3D.js
+++ js/3D.js
@@ -10,30 +10,28 @@ const scene = new THREE.Scene();
 const container = document.querySelector('#scene-container');
 
 // Create a camera
-const fov = 35; // AKA Field of View
+const fov = 50; // AKA Field of View
 const aspect = window.innerWidth / window.innerHeight;
 const near = 0.1; // the near clipping plane
-const far = 100; // the far clipping plane
-
+const far = 1000; // the far clipping plane
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-
-camera.position.set(0, 0, 10);
+camera.position.set(10, 0, 0);
 camera.lookAt(0, 0, 0);
 
-let controls
+const renderer = new THREE.WebGLRenderer({alpha: true},{antialias: true});
+renderer.setSize( container.clientWidth, container.clientHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 
-// create a geometry
-const geometry = new BoxBufferGeometry(2, 2, 2);
-// create a default (white) Basic material
-const material = new MeshBasicMaterial();
-// create a Mesh containing the geometry and material
-const cube = new Mesh(geometry, material);
-scene.add(cube);
+container.append(renderer.domElement);
+
+THREE.Cache.enabled = true;
 
 const loader = new GLTFLoader();
-loader.load( 'models/Horse.glb', 
-function ( gltf ) {
-  scene.add( gltf.scene );
+loader.load( 'models/Head/Head.glb', 
+(gltf) => {
+  const mesh = gltf.scene;
+  mesh.position.set(0,0.5,0);
+  scene.add( mesh );
 }, 
 function (xhr){
   console.log((xhr.loader / xhr.total * 100) + '% loader');
@@ -42,27 +40,18 @@ undefined, function ( error ) {
 	console.error( error );
 } );
 
-const loader2 = new GLTFLoader();
-loader2.load( 'models/Head/Head.glb', 
-function ( glb ) {
-  scene.add( glb.scene );
-}, );
-
-const topLight = new THREE.DirectionalLight('white', 10);
-topLight.position.set(500, 500, 500);
+const topLight = new THREE.DirectionalLight('white', 3);
+topLight.position.set(500, 200, 200);
 topLight.castShadow = true;
 scene.add(topLight);
 
-const ambientLight = new THREE.AmbientLight('white', 100);
+const ambientLight = new THREE.AmbientLight('white', 1);
 scene.add(ambientLight);
 
-//const renderer = new THREE.WebGLRenderer({alpha: true});
-const renderer = new THREE.WebGLRenderer({alpha: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-
-// function animate(){
-//   requestAnimationFrame(animate);
-// }
+function animate(){
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
 
 // window.addEventListener("resize", function(){
 //   camera.aspect = window.innerWidth / window.innerHeight;
@@ -70,10 +59,4 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 //   renderer.setSize(window.innerWidth, window.innerHeight);
 // });
 
-//animate();
-
-//render place
-container.append(renderer.domElement);
-//document.getElementById("scene-container").appendChild(renderer.domElement);
-
-renderer.render(scene, camera);
+animate();
